5b468b05c36559734a20dc1e52827612c21eeb15
nika-jobava481 <jobavanika481@gmail.com>
2025-12-29 02:13:40 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Main JavaScript file
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Movie Catalog website loaded');
+    
+    // Your JavaScript code goes here
+});
+
